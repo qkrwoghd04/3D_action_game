684e7c959c38bf100e000e0ec14fb2f26b57faae
JDEV <qkrwoghd04@naver.com>
2024-05-15 19:50:27 +0800

    initial commit

---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int mapSize = 31;
+    public GameObject obstaclePrefab;
+    public GameObject tallObstaclePrefab;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    void GenerateMap()
+    {
+        // Create base map plane
+        GameObject mapPlane = GameObject.CreatePrimitive(PrimitiveType.Plane);
+        mapPlane.transform.localScale = new Vector3(mapSize / 10.0f, 1, mapSize / 10.0f);
+        mapPlane.transform.position = new Vector3(mapSize / 2, 0, mapSize / 2);
+
+        // Generate obstacles
+        GenerateObstacles();
+    }
+
+    void GenerateObstacles()
+    {
+        int obstacleCount = 20;
+        int tallObstacleCount = 5;
+        int obstaclesGenerated = 0;
+        int tallObstaclesGenerated = 0;
+
+        while (obstaclesGenerated < obstacleCount || tallObstaclesGenerated < tallObstacleCount)
+        {
+            int x = Random.Range(0, mapSize);
+            int z = Random.Range(0, mapSize);
+
+            // Avoid placing obstacles in the center (Boss area)
+            if (x >= mapSize / 2 - 2 && x <= mapSize / 2 + 2 && z >= mapSize / 2 - 2 && z <= mapSize / 2 + 2)
+            {
+                continue;
+            }
+
+            // Choose obstacle type
+            GameObject obstacle;
+            if (tallObstaclesGenerated < tallObstacleCount && Random.value < 0.25f)
+            {
+                obstacle = Instantiate(tallObstaclePrefab);
+                obstacle.transform.position = new Vector3(x, 1, z);
+                tallObstaclesGenerated++;
+            }
+            else if (obstaclesGenerated < obstacleCount)
+            {
+                obstacle = Instantiate(obstaclePrefab);
+                obstacle.transform.position = new Vector3(x, 0.5f, z);
+                obstaclesGenerated++;
+            }
+        }
+    }
+}
